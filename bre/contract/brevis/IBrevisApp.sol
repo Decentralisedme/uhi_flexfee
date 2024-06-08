f37106402cea4aea329d7c071b9c55a55c7cc0ba
jpw993 <132077410+jpw993@users.noreply.github.com>
2024-06-08 13:26:51 +0100

    fix lib paths

--- bre/contract/brevis/IBrevisApp.sol
+++ bre/contract/brevis/IBrevisApp.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "../lib/Lib.sol";
+import "./Lib.sol";
 
 interface IBrevisApp {
     function brevisCallback(bytes32 _requestId, bytes calldata _appCircuitOutput) external;
--- bre/contract/brevis/IBrevisProof.sol
+++ bre/contract/brevis/IBrevisProof.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "../lib/Lib.sol";
+import "./Lib.sol";
 
 interface IBrevisProof {
     function submitProof(
