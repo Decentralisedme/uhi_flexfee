721974e5a1f228c1feaeb3448cf65013ddff0222
Rayan Mansur <rm2022@ic.ac.uk>
2024-06-10 11:38:54 +0100

    [FEAT] changing from receipts to storage

--- bre/app/src/index.ts
+++ bre/app/src/index.ts
@@ -1,6 +1,7 @@
-import { Brevis, ErrCode, ProofRequest, Prover, TransactionData, ReceiptData, Field } from 'brevis-sdk-typescript';
+import { Brevis, ErrCode, ProofRequest, Prover, TransactionData, ReceiptData, Field, StorageData } from 'brevis-sdk-typescript';
 import Web3, { Contract, ContractAbi, EventLog } from 'web3';
 import { readFileSync } from 'fs';
+import { RegisteredSubscription } from 'web3/lib/commonjs/eth.exports';
 
 
 const rootBrevisDirectory = __dirname.split('/').slice(0, -1).join('/')
@@ -17,6 +18,37 @@ var gasPrice, gasLimit;
 interface SubmitResponse {
     brevisId: string;
     fee: string;
+}
+
+async function getStorageDataAtBlock(chosenBlock: number, contractAddress: string, index: number, web3: Web3<RegisteredSubscription>) {
+    const storageVal = await web3.eth.getStorageAt(contractAddress, index, chosenBlock);
+
+    const storageData = new StorageData({
+        block_num: chosenBlock,
+        address: contractAddress,
+        slot: "0x0000000000000000000000000000000000000000000000000000000000000000", //TODO fix this up
+        value: storageVal
+    })
+
+    return storageData
+}
+
+async function getStorageDataPoints(numDataPoints: number, startingBlock: number, contractAddress: string, index: number, web3: Web3<RegisteredSubscription>){
+    var dataPoints = []
+
+    var curBlock = startingBlock;
+
+    console.log("hello")
+
+    while (dataPoints.length < numDataPoints) {
+        const storageItem = await getStorageDataAtBlock(curBlock, contractAddress, index, web3);
+        dataPoints.push(storageItem)
+        curBlock --;
+    }
+
+    console.log(dataPoints)
+    console.log(dataPoints.length)
+    return dataPoints;
 }
 
 function formatBrevisData(brevisRes: SubmitResponse, refundee: string, callback: string){
@@ -175,90 +207,17 @@ async function main() {
     const privateKey = process.argv[2]
     console.log(privateKey)
 
-    // const tempWeb3Addr = new Web3("https://eth.llamarpc.com")
-    // getPreviousLogs("0x125e0b641d4a4b08806bf52c0c6757648c9963bcda8681e4f996f09e00d4c2cc", "0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640", ABI, tempWeb3Addr)
-
-    // var receiptList = await getPreviousLogs("0x125e0b641d4a4b08806bf52c0c6757648c9963bcda8681e4f996f09e00d4c2cc", "0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640", USDC_WETH_V3_ABI, web3, 1500)
-
-    // console.log("got all receipts")
-
     const proofReq = new ProofRequest();
 
-    // // work backwards on receiptList
+    const storageDataList = await getStorageDataPoints(15, 19308799, "0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640", 0, new Web3("https://eth.llamarpc.com"))
 
-    // for (var i = receiptList.length - 1; i >= 0 ; i--) {
-    //     var indexPos = (receiptList.length - i) - 1;
-    //     console.log("indexPos", indexPos)
-    //     // push
-    //     proofReq.addReceipt(receiptList[i], indexPos);
-
-    //     console.log(JSON.stringify(receiptList[i], null, 2))
-
-    //     console.log("added receipt")
-
-    //     console.log("num receipts", proofReq.getReceipts().length)
-
-    //     if (proofReq.getReceipts().length >= 2) {
-    //         break;
-    //     }
-    // }
-    // 19308800
-    proofReq.addReceipt(
-        new ReceiptData({
-            block_num: 19308799,
-            tx_hash: '0xfe8c6f82c3da2c3ccf65623f52a9e0d4d5af825e7ae3c32c013988efb403949b',
-            fields: [
-                new Field({
-                    contract: '0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640',
-                    log_index: 153,
-                    event_id: '0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67',
-                    is_topic: false,
-                    field_index: 2,
-                    value: '1412045384449495693860528555476309',
-                }),               
-            ],
-        }), 0
-    );       
-
-    proofReq.addReceipt(
-        new ReceiptData({
-            block_num: 19308798,
-            tx_hash: '0xf5ff0afb6acc8124eb00231cf93b4c378d013a9b94adb0b1b1b75c4a2fd7f45f',
-            fields: [
-                new Field({
-                    contract: '0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640',
-                    log_index: 236,
-                    event_id: '0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67',
-                    is_topic: false,
-                    field_index: 2,
-                    value: '1422062131426387928667329689726225',
-                }),               
-            ],
-        }), 1
-    ); 
-    
-    proofReq.addReceipt(
-        new ReceiptData({
-            block_num: 19308798,
-            tx_hash: '0xf5ff0afb6acc8124eb00231cf93b4c378d013a9b94adb0b1b1b75c4a2fd7f45f',
-            fields: [
-                new Field({
-                    contract: '0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640',
-                    log_index: 236,
-                    event_id: '0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67',
-                    is_topic: false,
-                    field_index: 2,
-                    value: '1442062131426387928667329689726225',
-                }),               
-            ],
-        }), 2
-    ); 
+    for (let i = 0; i < storageDataList.length; i++){
+        proofReq.addStorage(storageDataList[0], i)
+    }
 
     console.log("going to prove")
 
     const proofRes = await prover.prove(proofReq);
-
-    // console.log(proofRes)
 
     // error handling
     if (proofRes.has_err) {
@@ -282,84 +241,17 @@ async function main() {
 
     console.log("\n\ngoing to go submit\n\n")
 
-    // private async _prepareQuery(
-    //     request: ProofRequest,
-    //     circuitInfo: AppCircuitInfo,
-    //     srcChainId: number,
-    //     dstChainId: number,
-    // ): Promise<PrepareQueryResponse> {
-    //     const req = new PrepareQueryRequest({
-    //         chain_id: srcChainId,
-    //         target_chain_id: dstChainId,
-    //         receipt_infos: request.getReceipts().map(r => this.buildReceiptInfo(r.data)),
-    //         storage_query_infos: request.getStorages().map(s => this.buildStorageInfo(s.data)),
-    //         transaction_infos: request.getTransactions().map(t => this.buildTransactionInfo(t.data)),
-    //         use_app_circuit_info: true,
-    //         app_circuit_info: circuitInfo,
-    //     });
-    //     const res = await this.client.PrepareQuery(req);
-    //     return res;
-    // }
-
-    // private buildReceiptInfo(data: ReceiptData): ReceiptInfo {
-    //     return new ReceiptInfo({
-    //         blk_num: data.block_num,
-    //         transaction_hash: data.tx_hash,
-    //         log_extract_infos: data.fields.map(f => {
-    //             return new LogExtractInfo({
-    //                 contract_address: f.contract,
-    //                 log_index: f.log_index,
-    //                 log_topic0: f.event_id,
-    //                 value_from_topic: f.is_topic,
-    //                 value_index: f.field_index,
-    //                 value: f.value,
-    //             });
-    //         }),
-    //     });
-    // }
-
-        
-
-    var rData = proofReq.getReceipts().map(r => {
-        var data = r.data
-       var x =  {
-                    blk_num: data.block_num,
-                    transaction_hash: data.tx_hash,
-                    log_extract_infos: data.fields.map(f => {
-                        return {
-                            contract_address: f.contract,
-                            log_index: f.log_index,
-                            log_topic0: f.event_id,
-                            value_from_topic: f.is_topic,
-                            value_index: f.field_index,
-                            value: f.value,
-                        };
-                    }),
-                }
-        console.log(x)
-        return data
-    })
-
-    var sData = proofReq.getStorages().map(s => s.data)
-    var tData = proofReq.getTransactions().map(t => t.data)
-
-    // console.log(JSON.stringify(rData, null, 2))
-    // console.log(JSON.stringify(sData, null, 2))
-    // console.log(JSON.stringify(tData, null, 2))
-    
-
-
     try {
         const brevisRes = await brevis.submit(proofReq, proofRes, 1, 11155111);
         console.log('brevis res', brevisRes);
 
-        const refundee = getAccount(privateKey);
-        const callback = "0xef1B4B164Fd3b7933bfaDb042373560e715Ec5D6";
+        // const refundee = getAccount(privateKey);
+        // const callback = "0xef1B4B164Fd3b7933bfaDb042373560e715Ec5D6";
 
-        var brevisDataToSend = (formatBrevisData(brevisRes, refundee, callback))
-        console.log(refundee, brevisRequestAddr, brevisDataToSend, privateKey)
+        // var brevisDataToSend = (formatBrevisData(brevisRes, refundee, callback))
+        // console.log(refundee, brevisRequestAddr, brevisDataToSend, privateKey)
 
-        await sendTransaction(refundee, brevisRequestAddr, brevisDataToSend, privateKey)
+        // await sendTransaction(refundee, brevisRequestAddr, brevisDataToSend, privateKey)
 
         await brevis.wait(brevisRes.brevisId, 11155111);
     } catch (err) {
@@ -367,5 +259,5 @@ async function main() {
     }
 }
 
-main();
 
+main()
