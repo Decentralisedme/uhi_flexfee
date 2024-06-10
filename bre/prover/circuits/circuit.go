a50994372c028d8fabd6b9b74ffe05cd4b134e0d
jpw993 <132077410+jpw993@users.noreply.github.com>
2024-06-10 12:38:34 +0100

    example using storage

--- bre/prover/circuits/circuit.go
+++ bre/prover/circuits/circuit.go
@@ -33,6 +33,11 @@ var UsdcPoolAddress = sdk.ConstUint248(
 var twoPower96 = sdk.ConstUint248("79228162514264337593543950336")
 var zeroU248 = sdk.ConstUint248(0)
 
+// var sqrtPriceMask = sdk.ConstUint248(
+// 	common.HexToAddress("0x000000000000000000000000ffffffffffffffffffffffffffffffffffffffff"))
+
+var sqrtPriceMask = sdk.ConstUint248("1461501637330902918203684832716283019655932542975")
+
 func (c *AppCircuit) Allocate() (maxReceipts, maxSlots, maxTransactions int) {
 	// Allocating regions for different source data. Here, we are allocating 5 data
 	// slots for "receipt" data, and none for other data types. Please note that if
@@ -40,7 +45,7 @@ func (c *AppCircuit) Allocate() (maxReceipts, maxSlots, maxTransactions int) {
 	// always have 5 processing "chips" for receipts and none for others. It means
 	// your compiled circuit will always be only able to process up to 5 receipts and
 	// cannot process other types unless you change the allocations and recompile.
-	return 3, 0, 0
+	return 0, 3, 0
 }
 
 func toBoolean(val sdk.Uint248, api *sdk.CircuitAPI) bool {
@@ -68,33 +73,46 @@ func absDiff(api *sdk.CircuitAPI, a, b sdk.Uint248) sdk.Uint248 {
 	return u248.Sub(a, b)
 }
 
-
 func (c *AppCircuit) Define(api *sdk.CircuitAPI, in sdk.DataInput) error {
 	u248 := api.Uint248
 
 	// In order to use the nice methods such as .Map() and .Reduce(), raw data needs
 	// to be wrapped in a DataStream. You could also use the raw data directly if you
 	// are familiar with writing gnark circuits.
-	receipts := sdk.NewDataStream(api, in.Receipts)
+	storageDataSlots := sdk.NewDataStream(api, in.StorageSlots)
 
 	// Main application logic: Run the assert function on each receipt. The function
 	// should return 1 if assertion successes and 0 otherwise
-	sdk.AssertEach(receipts, func(l sdk.Receipt) sdk.Uint248 {
-		assertionPassed := u248.And(
-			// Check that the contract address of each log field is the expected contract
-			u248.IsEqual(l.Fields[0].Contract, UsdcPoolAddress),
-			// Check the EventID of the fields are as expected
-			u248.IsEqual(l.Fields[0].EventID, EventIdSwap),
-			// Check the index of the fields are as expected
-			u248.IsZero(l.Fields[0].IsTopic),                     // `sqrtPriceX96` is not a topic field
-			u248.IsEqual(l.Fields[0].Index, sdk.ConstUint248(2)), // `sqrtPriceX96` is the index 2 data field in the `Swap` event
-		)
-		return assertionPassed
+	// sdk.AssertEach(receipts, func(l sdk.Receipt) sdk.Uint248 {
+	// 	assertionPassed := u248.And(
+	// 		// Check that the contract address of each log field is the expected contract
+	// 		u248.IsEqual(l.Fields[0].Contract, UsdcPoolAddress),
+	// 		// Check the EventID of the fields are as expected
+	// 		u248.IsEqual(l.Fields[0].EventID, EventIdSwap),
+	// 		// Check the index of the fields are as expected
+	// 		u248.IsZero(l.Fields[0].IsTopic),                     // `sqrtPriceX96` is not a topic field
+	// 		u248.IsEqual(l.Fields[0].Index, sdk.ConstUint248(2)), // `sqrtPriceX96` is the index 2 data field in the `Swap` event
+	// 	)
+	// 	return assertionPassed
+	// })
+
+	fmt.Println("jflkdsaf")
+
+	sqrtPriceX96s := sdk.Map(storageDataSlots, func(storageSlot sdk.StorageSlot) sdk.Uint248 {
+		// 248 - 160 = 96
+		storageVal := api.ToUint248(storageSlot.Value)
+
+		binary := api.Uint248.ToBinary(storageVal, 248)
+
+		maskedBinary := binary[0:159]
+		
+		resultingSqrtPrice := api.Uint248.FromBinary(maskedBinary...)
+		
+		return resultingSqrtPrice
 	})
 
-	sqrtPriceX96s := sdk.Map(receipts, func(receipt sdk.Receipt) sdk.Uint248 {
-		return api.ToUint248(receipt.Fields[0].Value)
-	})
+	fmt.Println("sqrtPrices")
+	sqrtPriceX96s.Show()
 
 	// convert sqrtPriceX96 to price
 	// price = (sqrtPriceX96 / (2 ^ 96)) ^ 2
@@ -104,7 +122,6 @@ func (c *AppCircuit) Define(api *sdk.CircuitAPI, in sdk.DataInput) error {
 	})
 
 	mean := sdk.Mean(prices)
-
 
 	// sqrd_variance := sdk.Map(prices, func(price sdk.Uint248) sdk.Uint248 {
 	// 	variance := u248.Sub(price, mean)
@@ -123,7 +140,6 @@ func (c *AppCircuit) Define(api *sdk.CircuitAPI, in sdk.DataInput) error {
 	// _circuitOutput: abi.encodePacked(uint256,uint248,uint64,address)
 	// this variable Salt isn't used anywhere. it's just here to demonstrate how to output bytes32/uint256
 	api.OutputUint(248, mean)
-	
 
 	return nil
 }
--- bre/prover/circuits/circuit_test.go
+++ bre/prover/circuits/circuit_test.go
@@ -17,40 +17,56 @@ func TestCircuit(t *testing.T) {
 	check(err)
 
 	usdcPool := common.HexToAddress("0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640")
-	swapEvent := common.HexToHash("0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67")
+	// swapEvent := common.HexToHash("0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67")
 
-	var num, _ = new(big.Int).SetString("1286977918479647748315046269489039", 10)
-	sqrtPriceX96_1 := common.BytesToHash(big.NewInt(num.Int64()).Bytes())
+	// var num, _ = new(big.Int).SetString("1286977918479647748315046269489039", 10)
+	// sqrtPriceX96_1 := common.BytesToHash(big.NewInt(num.Int64()).Bytes())
 
 	// Adding a receipt query into the querier
-	app.AddReceipt(sdk.ReceiptData{
+	// app.AddReceipt(sdk.ReceiptData{
+	// 	BlockNum: big.NewInt(19996326),
+	// 	TxHash:   common.HexToHash("0xb79c3ff11f9f402439915669be5d01f767e167c30ac24a09f66c69c0ed6cdaac"),
+	// 	Fields: [sdk.NumMaxLogFields]sdk.LogFieldData{
+	// 		{Contract: usdcPool, LogIndex: 12, EventID: swapEvent, IsTopic: false, FieldIndex: 2, Value: sqrtPriceX96_1}, // field: USDCPool.Swap.sqrtPriceX96
+	// 	},
+	// })
+
+	// num, _ = new(big.Int).SetString("1286977918479647748315046269489039", 10)
+	// sqrtPriceX96_2 := common.BytesToHash(big.NewInt(num.Int64()).Bytes())
+
+	// app.AddReceipt(sdk.ReceiptData{
+	// 	BlockNum: big.NewInt(19996331),
+	// 	TxHash:   common.HexToHash("0xae1e58570ff0d9fcf450814a9e467905c131cd7c654c2ef758dd0bc1d4267027"),
+	// 	Fields: [sdk.NumMaxLogFields]sdk.LogFieldData{
+	// 		{Contract: usdcPool, LogIndex: 3, EventID: swapEvent, IsTopic: false, FieldIndex: 2, Value: sqrtPriceX96_2}, // field: USDCPool.Swap.sqrtPriceX96
+	// 	},
+	// })
+
+	// num, _ = new(big.Int).SetString("1286980030786437829223832265541310", 10)
+	// sqrtPriceX96_3 := common.BytesToHash(big.NewInt(num.Int64()).Bytes())
+
+	// app.AddReceipt(sdk.ReceiptData{
+	// 	BlockNum: big.NewInt(19996331),
+	// 	TxHash:   common.HexToHash("0x4da85fde8c012f8fd6847e86e7114bc3c65731f315ee20abbf6c3b2f798439a7"),
+	// 	Fields: [sdk.NumMaxLogFields]sdk.LogFieldData{
+	// 		{Contract: usdcPool, LogIndex: 3, EventID: swapEvent, IsTopic: false, FieldIndex: 2, Value: sqrtPriceX96_3}, // field: USDCPool.Swap.sqrtPriceX96
+	// 	},
+	// })
+
+	app.AddStorage(sdk.StorageData{
 		BlockNum: big.NewInt(19996326),
-		TxHash:   common.HexToHash("0xb79c3ff11f9f402439915669be5d01f767e167c30ac24a09f66c69c0ed6cdaac"),
-		Fields: [sdk.NumMaxLogFields]sdk.LogFieldData{
-			{Contract: usdcPool, LogIndex: 12, EventID: swapEvent, IsTopic: false, FieldIndex: 2, Value: sqrtPriceX96_1}, // field: USDCPool.Swap.sqrtPriceX96
-		},
+		Address:  usdcPool,
+		Slot:     common.HexToHash("0x0"),
+		Value: 	  common.HexToHash("0x00010002d302d3006802f57f0000000000003f73f1eef341535bdd27ba150f8f"),
+		// Value:    common.HexToHash("0x00010002d302d3006802f57f0000000000003f73f1eef341535bdd27ba150f8f"),
 	})
 
-	num, _ = new(big.Int).SetString("1286977918479647748315046269489039", 10)
-	sqrtPriceX96_2 := common.BytesToHash(big.NewInt(num.Int64()).Bytes())
-
-	app.AddReceipt(sdk.ReceiptData{
+	app.AddStorage(sdk.StorageData{
 		BlockNum: big.NewInt(19996331),
-		TxHash:   common.HexToHash("0xae1e58570ff0d9fcf450814a9e467905c131cd7c654c2ef758dd0bc1d4267027"),
-		Fields: [sdk.NumMaxLogFields]sdk.LogFieldData{
-			{Contract: usdcPool, LogIndex: 3, EventID: swapEvent, IsTopic: false, FieldIndex: 2, Value: sqrtPriceX96_2}, // field: USDCPool.Swap.sqrtPriceX96
-		},
-	})
-
-	num, _ = new(big.Int).SetString("1286980030786437829223832265541310", 10)
-	sqrtPriceX96_3 := common.BytesToHash(big.NewInt(num.Int64()).Bytes())
-
-	app.AddReceipt(sdk.ReceiptData{
-		BlockNum: big.NewInt(19996331),
-		TxHash:   common.HexToHash("0x4da85fde8c012f8fd6847e86e7114bc3c65731f315ee20abbf6c3b2f798439a7"),
-		Fields: [sdk.NumMaxLogFields]sdk.LogFieldData{
-			{Contract: usdcPool, LogIndex: 3, EventID: swapEvent, IsTopic: false, FieldIndex: 2, Value: sqrtPriceX96_3}, // field: USDCPool.Swap.sqrtPriceX96
-		},
+		Address:  usdcPool,
+		Slot:     common.HexToHash("0x0"),
+		Value:    common.HexToHash("0x00010002d302d3006802f57f0000000000003f73f8c2359ddd1b7c9f5c6dc6be"),
+		// Value:    common.HexToHash("0x00010002d302d3006802f57f0000000000003f73f8c2359ddd1b7c9f5c6dc6be"),
 	})
 
 	// Initialize our AppCircuit and prepare the circuit assignment
